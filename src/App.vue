480d61e63bcb40eb8b5016e2f1f983ebf9744611
Denial <den4ic369@gmail.com>
2023-03-19 12:52:58 +0300

    hm1 done

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Counters from "./components/Counters.vue";
+</script>
+
+<template>
+  <h1>Ohae</h1>
+  <Counters></Counters>
+</template>
+
+
+
+<style scoped>
+  h1{
+    color: brown;
+    text-align: center;
+  }
+</style>
---
+++ src/components/Counters.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive, ref, watch } from 'vue';
+const counterReactive = reactive({
+    counter1: 0, counter2: 0,
+});
+const date = ref(new Date().toUTCString());
+
+// function counterHandler(event){
+//     console.log();
+// }
+
+    watch(() => {
+        setInterval(()=>{date.value = new Date().toUTCString()}, 1000)
+    });
+const counterRef = ref(0);
+</script>
+
+<template>
+    <button @click="counterReactive .counter1++">Plus</button>
+    <h3>{{ counterReactive.counter1 }}</h3>
+    <button @mousemove="counterReactive .counter2 = $event.clientX">MouseMove</button>
+    <h3>{{ counterReactive.counter2 }}</h3>
+    <button @click="counterRef++">Plus {{ counterRef }}</button>
+
+    <h1>{{ date }}</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
